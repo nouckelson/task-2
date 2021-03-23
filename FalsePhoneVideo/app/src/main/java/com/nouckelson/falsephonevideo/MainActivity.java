840f64a98a7acb69a8c8d14dd5edd37664b18370
unknown <jnouckelson@hotmail.com>
2021-03-23 14:29:19 -0400

    False Phone Video  Project

---
+++ FalsePhoneVideo/app/src/main/java/com/nouckelson/falsephonevideo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.nouckelson.falsephonevideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+        TextView tvMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvMessage=findViewById(R.id.tvMassage);
+        if(getIntent().getData()!=null){
+            tvMessage.setText(getIntent().getData().toString());
+        }
+        else{
+            tvMessage.setText("No data received!");
+        }
+    }
+}
